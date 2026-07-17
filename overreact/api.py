c9ea7cf10aa5ca9f1a0ee435baec9b35c4763b20
Matheus Rauen <matheus.rauen@grad.ufsc.br>
2026-07-17 18:04:04 -0300

    feat: adding reactants separation based on reaction scheme

--- overreact/api.py
+++ overreact/api.py
@@ -21,6 +21,7 @@ __all__ = [
 
 import logging
 import warnings
+import re
 from typing import TYPE_CHECKING
 
 import numpy as np
@@ -774,20 +775,15 @@ def get_kdiff(
     compounds: dict | None = None,
     environment: str | None = None,
     radii: list[float] | None = None,
-    is_bimolecular: list[bool] | None = None,
+    bimolecular: list[str] | None = None,
     temperature: float = 298.15,
     pressure: float = constants.atm,
 ) -> float:
-    print("scheme.A shape =", np.asarray(scheme.A).shape)
-    print("number of reactions =", len(scheme.reactions))
-    print('before:{}'.format(is_bimolecular))
-    print('reactions:{}'.format(scheme.reactions))
-    
     if radii is None:
         radii = []
         
-    if is_bimolecular is None:
-        is_bimolecular = []
+    if bimolecular is None:
+        bimolecular = []
     
     for name in compounds: 
         if environment is None:
@@ -797,52 +793,37 @@ def get_kdiff(
     if compounds is not None:
         compounds = rx.io._check_compounds(compounds)
        
-    for rx_id, column in enumerate(zip(*scheme.A)):
-        count_reactant = sum(1 for value in column if value < 0)
-        if count_reactant == 2:
-            print("""
-                  reaction: {}
-                  reactant counter: {}
-                  """.format(rx_id, count_reactant))
-            reactant_indices = [
-                i for i, val in enumerate(column)
-                if val < 0
-            ]
-
-            reactant_names = [
-                scheme.compounds[i]
-                for i in reactant_indices
-            ] 
+    for reaction in (scheme.reactions): 
+        reactants = re.split('-> | <=>', reaction)[0]
+        for spc in reactants.strip().split('+'):
+            bimolecular.extend(spc)
             
-            # reactant_indices = (id for id, val in enumerate(column) if val < 0)
-            # reactant_names = (scheme.compounds[indice] for indice in reactant_indices)
-            radii_reactant = [
-                coords.get_molecular_radius(
-                    atomnos=compounds[reactant].atomnos,
-                    atomcoords=compounds[reactant].atomcoords,
-                ) for reactant in reactant_names
-            ]
-            radii.append(radii_reactant)
-            is_bimolecular.append(True)
-        elif count_reactant == 1:
-            logger.warning("skipping diffusion rate constant calculation (unimolecular reaction)")
-            radii.append(None)
-            is_bimolecular.append(False)
-        elif count_reactant > 2:
-            logger.warning("skipping diffusion rate constant calculation (reaction with more than 2 reactants)")
-            radii.append(None)
-            is_bimolecular.append(False)
-       
-    print('after:{}'.format(is_bimolecular))
+    #         radii_reactant = [
+    #             coords.get_molecular_radius(
+    #                 atomnos=compounds[reactant].atomnos,
+    #                 atomcoords=compounds[reactant].atomcoords,
+    #             ) for reactant in reactant_names
+    #         ]
+    #         radii.append(radii_reactant)
+    #         is_bimolecular.append(True)
+    #     elif count_reactant == 1:
+    #         logger.warning("skipping diffusion rate constant calculation (unimolecular reaction)")
+    #         radii.append(None)
+    #         is_bimolecular.append(False)
+    #     elif count_reactant > 2:
+    #         logger.warning("skipping diffusion rate constant calculation (reaction with more than 2 reactants)")
+    #         radii.append(None)
+    #         is_bimolecular.append(False)
       
-    kdiff, diffusion_method = rates.collins_kimball(
-        radii=radii,
-        is_bimolecular=is_bimolecular,
-        viscosity=environment,
-        temperature=temperature,
-        pressure=pressure,
-    )
-    return kdiff, diffusion_method
+    # kdiff, diffusion_method = rates.collins_kimball(
+    #     radii=radii,
+    #     is_bimolecular=is_bimolecular,
+    #     viscosity=environment,
+    #     temperature=temperature,
+    #     pressure=pressure,
+    # )
+    # return kdiff, diffusion_method
+    pass
 
 def get_kobs(
     k_tst: float | np.ndarray,
